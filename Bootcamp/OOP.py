7a6a51eb0a3bc06dbefbeb5e3215b9b918060ddc
Yaroslav <yaros6666@yandex.ru>
2022-08-16 18:18:05 +0300

    seaborn

--- Bootcamp/OOP.py
+++ Bootcamp/OOP.py
@@ -1,45 +1,108 @@
-import turtle
-# from turtle import *
-from random import randint
+# import turtle
+#
+# import random
+#
+# rand = random.random()
+# rand.
+# rand1 = random.randint(1,20)
+#
+# finish = 200
+#
+# t1 = turtle.Turtle()   # Создаем объект класса Turtle
+#
+# t1.shape('turtle')
+# t1.color('green')
+# t1.penup()
+# t1.goto(-200,20)
+# t1.pendown()
+#
+# t2 = turtle.Turtle()   # Создаем объект класса Turtle
+#
+# t2.shape('turtle')
+# t2.color('red')
+# t2.penup()
+# t2.goto(-200,-20)
+# t2.pendown()
+#
+# def razmetkf():
+#     temp = turtle.Turtle()
+#     temp.speed(0)
+#     for i in range(1,21):
+#         temp.penup()
+#         temp.goto(-200+i*20, 50)
+#         temp.pendown()
+#         temp.goto(-200 + i * 20, -100)
+#     temp.hideturtle()
+#
+# razmetkf()
+#
+# x1,y1= t1.position()
+# x2,y1= t2.position()
+#
+# while(x1<finish and x2<finish):
+#
+#     t1.speed(random.randint(0,10))
+#     t1.forward(random.randint(1,30))
+#     x1, y1 = t1.position()
+#     if(x1>finish):break
+#
+#     t2.speed(random.randint(0,10))
+#     t2.forward(random.randint(1,30))
+#     x2, y1 = t2.position()
+#
+# turtle.exitonclick()
+#
+#
+
+import matplotlib.pyplot as plt
+# import seaborn as sns
+#
+#
+# x = ['А', 'Б', 'В']
+# y = [10, 50, 30]
+#
+# sns.barplot(x=x, y=y);
+# input('Press ENTER to exit')
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+#
+# def fahrenheit2celsius(temp):
+#     """
+#     Returns temperature in Celsius.
+#     """
+#     return (5. / 9.) * (temp - 32)
+#
+#
+# def convert_ax_c_to_celsius(ax_f):
+#     """
+#     Update second axis according with first axis.
+#     """
+#     y1, y2 = ax_f.get_ylim()
+#     ax_c.set_ylim(fahrenheit2celsius(y1), fahrenheit2celsius(y2))
+#     ax_c.figure.canvas.draw()
+#
+# fig, ax_f = plt.subplots()
+# ax_c = ax_f.twinx()
+#
+# # automatically update ylim of ax2 when ylim of ax1 changes.
+# ax_f.callbacks.connect("ylim_changed", convert_ax_c_to_celsius)
+# ax_f.plot(np.linspace(-40, 120, 100))
+# ax_f.set_xlim(0, 100)
+#
+# ax_f.set_title('Two scales: Fahrenheit and Celsius')
+# ax_f.set_ylabel('Fahrenheit')
+# ax_c.set_ylabel('Celsius')
+#
+# plt.show()
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
+x = ['А', 'Б', 'В']
+y = [10, 50, 30]
 
-finish = 200
-
-t1 = turtle.Turtle()   # Создаем объект класса Turtle
-
-t1.shape('turtle')
-t1.color('green')
-t1.penup()
-t1.goto(-500,500)
-t1.pendown()
-
-t2 = turtle.Turtle()   # Создаем объект класса Turtle
-
-t2.shape('turtle')
-t2.color('red')
-t2.penup()
-t2.goto(-500,400)
-t2.pendown()
-
-def razmetkf():
-    temp = turtle.Turtle()
-    temp.speed(0)
-    for i in range(21):
-        temp.penup()
-        temp.goto(-200+i*20, 50)
-        temp.pendown()
-        temp.goto(-200 + i * 20, -100)
-    temp.hideturtle()
-
-
-
-
-turtle.exitonclick()
-
-
-
-
-
-
-
+sns.barplot(x=x, y=y);
+plt.show()
