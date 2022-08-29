7721ea6b67b0cea3758ee4806203a168e49941a5
Yaroslav <yaros6666@yandex.ru>
2022-08-29 16:02:58 +0300

    7

--- HackerRank/String/string.py
+++ HackerRank/String/string.py
@@ -103,84 +103,96 @@
 
 # N = int(input())
 
-N =17
+# N =17
+#
+# res_binar = ''
+# a = N
+# while a >= 2:
+#     c = str(a % 2)
+#     res_binar = c + res_binar
+#     a = a // 2
+#
+# temp = '1' + res_binar
+# res_binar = ''
+#
+# len_line= len(temp)
+# x=2
+# y=len_line+1
+# z=len_line+1
+# v=len_line+1
+#
+# res_octal= ''
+# res_hex = ''
+# for i in range(1, N+1):
+#     a=i
+#     while a >= 2:
+#         c = str(a % 2)
+#         res_binar = c + res_binar
+#         a = a // 2
+#
+#     binar = '1' + res_binar
+#     res_binar =''
+#
+#     b = i
+#     while b >= 8:
+#         c = str(b % 8)
+#         res_octal = c + res_octal
+#         b = b // 8
+#
+#     octal = str(b) + res_octal
+#     res_octal = ''
+#
+#     if i < 10: d=i
+#     if i==10: d='A'
+#     if i == 11: d = 'B'
+#     if i == 12: d = 'C'
+#     if i == 13: d = 'D'
+#     if i == 14: d = 'E'
+#     if i == 15: d = 'F'
+#     if i>15:
+#         d = i
+#         while d >= 16:
+#             c = str(d % 16)
+#             if c == '10': c = 'A'
+#             if c == '11': c = 'B'
+#             if c == '12': c = 'C'
+#             if c == '13': c = 'D'
+#             if c == '14': c = 'E'
+#             if c == '15': c = 'F'
+#             res_hex = c + res_hex
+#             d = d // 16
+#
+#
+#
+#     hexad = str(d) + res_hex
+#     res_hex = ''
+#
+#
+#
+#     print(str(i).rjust(x) + octal.rjust(y)+hexad.rjust(z)  + binar.rjust(z))
+#
 
-res_binar = ''
-a = N
-while a >= 2:
-    c = str(a % 2)
-    res_binar = c + res_binar
-    a = a // 2
+# task 7
 
-temp = '1' + res_binar
-res_binar = ''
+# N = 3
+# alhabet = '0abcdefghijklmnopqrstuvwxyz'
+#
+# width_line = (N*2-1)+(N*2-2)
+#
+# for i in range(N,0,-1):
+#     print(alhabet[i].center(width_line,'-'))
+#
+#
 
-len_line= len(temp)
-x=2
-y=len_line+1
-z=len_line+1
-v=len_line+1
+def print_rangoli(size):
+    a = size   #Created 'a' variable for my convenience.
+    l = []
+    for i in range(a):
+        d = '-'*(a * 2-(2*(1+i)))+chr(96+a)
+        for j in range(i):
+            d += '-' + chr(96 + a - j - 1)
+        l.append((d+d[-2::-1]))
+    print('\n'.join(l+l[-2::-1]))
 
-
-
-
-
-
-res_octal= ''
-res_hex = ''
-for i in range(1, N+1):
-    a=i
-    while a >= 2:
-        c = str(a % 2)
-        res_binar = c + res_binar
-        a = a // 2
-
-    binar = '1' + res_binar
-    res_binar =''
-
-    b = i
-    while b >= 8:
-        c = str(b % 8)
-        res_octal = c + res_octal
-        b = b // 8
-
-    octal = str(b) + res_octal
-    res_octal = ''
-
-    if i < 10: d=i
-    if i==10: d='A'
-    if i == 11: d = 'B'
-    if i == 12: d = 'C'
-    if i == 13: d = 'D'
-    if i == 14: d = 'E'
-    if i == 15: d = 'F'
-    if i>15:
-        d = i
-        while d >= 16:
-            c = str(d % 16)
-            if c == '10': c = 'A'
-            if c == '11': c = 'B'
-            if c == '12': c = 'C'
-            if c == '13': c = 'D'
-            if c == '14': c = 'E'
-            if c == '15': c = 'F'
-            res_hex = c + res_hex
-            d = d // 16
-
-
-
-    hexad = str(d) + res_hex
-    res_hex = ''
-
-
-
-    print(str(i).rjust(x) + octal.rjust(y)+hexad.rjust(z)  + binar.rjust(z))
-
-
-
-
-
-
-
-
+print_rangoli(4)
 
